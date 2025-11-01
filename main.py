51e2f6f6a1a5cdec2b51adcd76f297e69982ed27
Kushal Khivasara <kushal.khivasara@outlook.com>
2025-11-01 22:42:22 +0530

    Base HTML template ready, used render_template function with a parameter

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+tasklist = [["Walk Dog", False], ["Wash Dishes", True], ["Take out trash", True]]
+
 @app.route('/')
 def home():
-    return render_template("./tasklist.html")
+    return render_template("./tasklist.html", tasklist = tasklist)
 
 app.run()
